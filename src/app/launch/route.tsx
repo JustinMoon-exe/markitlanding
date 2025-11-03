f394ac94d7faabf61bb472619d5fa320a8908e99
Hamatito <hamatito75@yahoo.com>
2025-11-02 20:22:56 -0500

    remove unused functions and variableS

--- src/app/launch/route.tsx
+++ src/app/launch/route.tsx
@@ -1,15 +1,14 @@
 // src/app/launch/route.ts
 import { NextRequest, NextResponse } from "next/server";
 
-function isMacOS(ua: string) { return /\bMacintosh;/.test(ua); }
+// function isMacOS(ua: string) { return /\bMacintosh;/.test(ua); }
 
 export async function GET(req: NextRequest) {
-  const ua = req.headers.get("user-agent") || "";
   const url = new URL(req.url);
-
   const fragment = url.hash || ""; // like "#code=...&verify=..."
-  // set up Mac compatibility
 
+  // set up Mac compatibility
+  // const ua = req.headers.get("user-agent") || "";
   // if (isMacOS(ua)) {
   //   // macOS: Universal Link path your app claims via AASA
   //   return NextResponse.redirect(`https://markittrade.com/ulink/auth${fragment}`, 302);
