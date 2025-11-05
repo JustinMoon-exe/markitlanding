6e0e69343cbb2645811b814efb58d9a31cdc98eb
Hamatito <hamatito75@yahoo.com>
2025-11-04 19:05:07 -0500

    turn to env variables

--- src/app/lib/firebase_init.tsx
+++ src/app/lib/firebase_init.tsx
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase-admin/firestore";
 let app: App | null = null;
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAglUEgCWfvRokwThWfHjfsa23O0yluqs4",
-  authDomain: "markit-aff0e.web.app",
-  projectId: "markit-aff0e",
-  storageBucket: "markit-aff0e.firebasestorage.app",
-  messagingSenderId: "622595107704",
-  appId: "1:622595107704:web:d3209a4ef1418e9c7ef68d",
-  measurementId: "G-50P8B6HBF8"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 export function getAdminApp(): App {
--- src/app/sso/page.tsx
+++ src/app/sso/page.tsx
@@ -13,14 +13,14 @@ import {
   User
 } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAglUEgCWfvRokwThWfHjfsa23O0yluqs4",
-  authDomain: "markittrade.com",
-  projectId: "markit-aff0e",
-  storageBucket: "markit-aff0e.firebasestorage.app",
-  messagingSenderId: "622595107704",
-  appId: "1:622595107704:web:d3209a4ef1418e9c7ef68d",
-  measurementId: "G-50P8B6HBF8"
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN_CUSTOM!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 export default function SSOPage() {
